27dd12ea6f56204fbea347da65d6b198bc55afea
jschoi <jschoi@wemb.co.kr>
2019-11-01 18:00:30 +0900

    src commit

---
+++ src/main/java/com/wemb/tim/oauth2_demo/config/AuthorizationConfig.java
@@ -0,0 +1,33 @@
+package com.wemb.tim.oauth2_demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationConfig extends AuthorizationServerConfigurerAdapter {
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory()
+                .withClient("bridgex")
+                .secret("wemb1234")
+                .scopes("read:current_user", "read:users")
+                .authorizedGrantTypes("client_credentials");
+    }
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.tokenStore(tokenStore());
+    }
+    @Bean
+    public TokenStore tokenStore() {
+
+        return new InMemoryTokenStore();
+    }
+
+}
